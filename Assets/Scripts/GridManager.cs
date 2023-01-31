eb02ffde5a829d1887cb9d5e9e3ef98c319c3f61
Timeitgame <113610844+Timeitgame@users.noreply.github.com>
2023-01-31 09:31:41 +0300

    Перенос камеры на место

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -21,7 +21,7 @@ public class GridManager : MonoBehaviour
             for (int y = 0; y < _height; y++)
             {
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
-                spawnedTile.name = $"Tile {x} {y}";// Normis name in the Hierarchy
+                spawnedTile.name = $"Tile {x} {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
