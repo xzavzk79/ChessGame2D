16bea4310c1a36b4c44455f21f2764baf5c55fe2
Timeitgame <113610844+Timeitgame@users.noreply.github.com>
2023-03-13 17:06:47 +0300

    Свап сторон. Определение вражеских фигур, свободных клеток, и друзей

--- Assets/Scripts/Pieces/BasePieces.cs
+++ Assets/Scripts/Pieces/BasePieces.cs
@@ -75,9 +75,21 @@ public class BasePieces : EventTrigger
 
         //Проверка клеток
         for(int i = 1; i <= movement; i++) 
-        { 
+        {   
             currentX += xDirection;
             currentY += yDirection;
+
+            CellState cellState = CellState.None;
+            cellState = mCurrentCell.mBoard.ValidateCell(currentX, currentY, this);
+
+            if (cellState == CellState.Enemy)
+            {
+                mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[currentX, currentY]);
+                break;
+            }
+
+            if (cellState != CellState.Free)
+                break;
 
             mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[currentX, currentY]);
             Debug.Log(currentY);
@@ -174,6 +186,8 @@ public class BasePieces : EventTrigger
         }
 
         Move();
+
+        mPieceManager.SwitchSides(mColor);
     }
     #endregion
 }
