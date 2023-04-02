d77c351664d1f2f75e2472dfba20d410fce7b2a0
DR3AM1NGs <ordo3202@gmail.com>
2023-04-03 00:41:52 +0300

    KnightComplete
    Сделаны ходы коня(knight)

--- Assets/Scripts/Pieces/Knight.cs
+++ Assets/Scripts/Pieces/Knight.cs
@@ -11,4 +11,42 @@ public class Knight : BasePieces
 
         GetComponent<Image>().sprite = Resources.Load<Sprite>("Knight");
     }
+
+    private void CreateCellPath(int flipper)
+    {
+        
+        //позиция фигуры
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+
+        //Влево
+        MatchesState(currentX - 2, currentY + (1 * flipper));
+
+        //Лево Верх
+        MatchesState(currentX-1, currentY + (2 * flipper));
+
+        //Право Верх
+        MatchesState(currentX + 1, currentY + (2 * flipper));
+
+        //Вправо
+        MatchesState(currentX + 2, currentY + (1 * flipper));
+    }
+    protected override void CheckPathing()
+    {
+        //Рисуем верхнуюю половину ходов
+        CreateCellPath(1);
+        //Рисуем нижнюю половину ходов
+        CreateCellPath(-1);
+    }
+
+    private void MatchesState(int targetX, int targetY)
+    {
+        CellState cellState = CellState.None;
+        cellState = mCurrentCell.mBoard.ValidateCell(targetX, targetY, this);
+
+        if (cellState != CellState.Friendly && cellState != CellState.OutOfBounds)
+        {
+            mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+        }
+    }
 }
