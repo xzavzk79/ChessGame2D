bdc31c66db5c8393f43e5efd7a584b4fb84c637e
DR3AM1NGs <ordo3202@gmail.com>
2023-06-16 05:28:16 +0300

    wBuild
    проверяй макс, у меня всё забилдилось

--- Assets/Scripts/PieceRCh.cs
+++ Assets/Scripts/PieceRCh.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PieceRCh : MonoBehaviour
--- Assets/Scripts/Pieces/King.cs
+++ Assets/Scripts/Pieces/King.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
